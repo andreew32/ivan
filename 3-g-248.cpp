10ab02caa86a5d15c015e7fdf5efa974d8973295
andreew32 <66839175+andreew32@users.noreply.github.com>
2023-03-20 21:44:38 +0500

    3 done

--- 3-g-248.cpp
+++ 3-g-248.cpp
@@ -1,71 +1,165 @@
 #include <iostream>
 #include <cmath>
 
+#define _USE_MATH_DEFINES
+int glob = 0;
 class Point {
 private:
-    int x, y;
+    double x, y;
     double r, a;
-    static int partition(Point vec[], int lt, int rt) {
-        Point pivot(vec[rt]);
-        int i = lt - 1;
-        for (int j = lt; j <= rt - 1; j++) {
-            if (vec[j] < pivot) {
+    int index;
+    void debug() {
+        std::cout << x << " " << y << " "
+                  << r << " " << a << " "
+                  << index + 1 << std::endl;
+    }
+    static void debugar(Point arr[], int n) {
+        for (int i = 0; i < n; i++) {
+            arr[i].debug();
+            std::cout << std::endl;
+        }
+    }
+    static int partition(Point arr[], int start, int end) {
+        std::cout << "glob " << glob << std::endl;
+        glob++;
+        Point pivot = Point(arr[start]);
+        pivot.debug();
+        arr[start].debug();
+        int count = 0;
+        for (int i = start + 1; i <= end; i++) {
+            if (arr[i] <= pivot)
+                count++;
+        }
+
+        // Giving pivot element its correct position
+        int pivotIndex = start + count;
+        std::swap(arr[pivotIndex], arr[start]);
+
+        // Sorting left and right parts of the pivot element
+        int i = start, j = end;
+
+        while (i < pivotIndex && j > pivotIndex) {
+
+            while (arr[i] <= pivot) {
                 i++;
-                std::swap(vec[i], vec[j]);
+            }
+
+            while (arr[j] > pivot) {
+                j--;
+            }
+
+            if (i < pivotIndex && j > pivotIndex) {
+                std::swap(arr[i++], arr[j--]);
             }
         }
-        std::swap(vec[i + 1], vec[rt]);
-        return i + 1;
+
+        return pivotIndex;
     }
-    static void quickSort(Point vec[], int lt, int rt) {
-        if (lt < rt) {
-            int mid = partition(vec, lt, rt);
-            quickSort(vec, lt, mid - 1);
-            quickSort(vec, mid + 1, rt);
-        }
+
+    static void quickSort(Point arr[], int start, int end)
+    {
+
+        // base case
+        if (start >= end)
+            return;
+
+        // partitioning the array
+        int p = partition(arr, start, end);
+
+        // Sorting the left part
+        quickSort(arr, start, p - 1);
+
+        // Sorting the right part
+        quickSort(arr, p + 1, end);
     }
-    friend bool operator < (Point const& p1, Point const& p2) {
-        return p1.a < p2.a;
+
+    friend bool operator <= (Point const& p1, Point const& p2) {
+        return p1.a <= p2.a;
     }
-    void polarPoint() {
-        r = sqrt(x * x + y * y);
-        a = atan2(y,x);
+    friend bool operator > (Point const& p1, Point const& p2) {
+        return p1.a > p2.a;
     }
-public:
-    Point(const Point& p) {
-        x = p.x;
-        y = p.y;
-        r = p.r;
-        a = p.a;
-    }
-    Point(int x, int y) {
+    void init(double x, double y) {
         this->x = x;
         this->y = y;
-        polarPoint();
+    }
+public:
+    Point(Point const& p) {
+        this->x = p.x;
+        this->y = p.y;
+        this->r = p.r;
+        this->a = p.a;
+        this->index = p.index;
+    }
+    Point(double x, double y) {
+        init(x, y);
+    }
+    Point(double x, double y, unsigned i) {
+        init(x, y);
+        this->index = i;
     }
     Point() {}
-    void get(int& x, int& y) {
+    void polarPoint(Point& c) {
+        double x = this->x - c.x;
+        double y = this->y - c.y;
+        r = sqrt(x * x + y * y);
+        a = atan2(y,x);
+        if (a < 0)
+            a += 2 * M_PI;
+    }
+    void get(double& x, double& y) {
         x = this->x;
         y = this->y;
     }
     static void sort(Point vec[], int n) {
         quickSort(vec, 0, n - 1);
     }
+    static Point average(Point vec[], int n) {
+        double xs = 0, ys = 0;
+        for (unsigned i = 0; i < n; i++) {
+            double x, y;
+            vec[i].get(x, y);
+            xs += x;
+            ys += y;
+        }
+        return Point(xs/(n*1.0), ys/(n*1.0));
+    }
+    unsigned i() {
+        return this->index;
+    }
+    void setIndex(unsigned i) {
+        this->index = i;
+    }
 };
-int main()
-{
+int main() {
     unsigned n;
     std::cin >> n;
     Point* p = new Point[n];
     for (unsigned i = 0; i < n; i++) {
         int x, y;
         std::cin >> x >> y;
-        p[i] = Point(x, y);
+        p[i] = Point(x, y, i + 1);
     }
-    sort(p, n);
     for (unsigned i = 0; i < n; i++) {
-        p
+        std::cout << p[i].i() << " ";
     }
+    std::cout << std::endl;
+    Point center = Point::average(p, n);
+    for (unsigned i = 0; i < n; i++) {
+        std::cout << p[i].i() << " ";
+    }
+    std::cout << std::endl;
+    for (unsigned i = 0; i < n; i++)
+        p[i].polarPoint(center);
+    for (unsigned i = 0; i < n; i++) {
+        std::cout << p[i].i() << " ";
+    }
+    std::cout << std::endl;
+    Point::sort(p, n);
+    for (unsigned i = 0; i < n; i++) {
+        std::cout << p[i].i() << " ";
+    }
+    std::cout << std::endl;
     delete[] p;
     return 0;
 }
