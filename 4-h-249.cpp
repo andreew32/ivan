66cb26f0de8be79a58c7fcf774151409600629d6
andreew32 <66839175+andreew32@users.noreply.github.com>
2023-03-25 15:26:29 +0500

    task 4, classes added, divide in memory/time approach

--- 4-h-249.cpp
+++ 4-h-249.cpp
@@ -1,38 +1,118 @@
 #include <iostream>
+#include <cstdint>
+
+class Vect
+{
+private:
+    int32_t* a;
+    static uint16_t partition(int32_t arr[], uint16_t start, uint16_t end)
+    {
+        int32_t pivot = arr[start];
+        uint16_t count = 0;
+        for (uint16_t i = start + 1; i <= end; i++) {
+            if (arr[i] <= pivot)
+                count++;
+        }
+
+        //задаем осевому элементу его правильную позицию
+        uint16_t pivotIndex = start + count;
+        std::swap(arr[pivotIndex], arr[start]);
+
+        //сортируем по левую и по правую сторону от осевого элемента
+        uint16_t i = start, j = end;
+
+        while (i < pivotIndex && j > pivotIndex)
+        {
+            while (arr[i] <= pivot) {
+                i++;
+            }
+
+            while (arr[j] > pivot) {
+                j--;
+            }
+
+            if (i < pivotIndex && j > pivotIndex) {
+                std::swap(arr[i++], arr[j--]);
+            }
+        }
+
+        return pivotIndex;
+    }
+
+    //функция быстрой сортировки
+    static void quickSort(int32_t arr[], uint16_t start, uint16_t end)
+    {
+
+        //базовый случай
+        if (start >= end)
+            return; //сразу выходим из сортировки
+
+        //разбиваем массив
+        uint16_t p = partition(arr, start, end);
+
+        //сортируем левую часть
+        quickSort(arr, start, p - 1);
+
+        //сортируем правую часть
+        quickSort(arr, p + 1, end);
+    }
+public:
+    uint16_t l;
+    int32_t& operator[](uint16_t i) {return a[i];}
+    void allocate() {
+        a = new int32_t[l];
+    }
+    Vect() {}
+    ~Vect() {delete[] a;}
+    sort() {quickSort(a, 0, l - 1);}
+};
+
+class LongVect
+{
+
+};
 
 int main() {
-    unsigned n; //создание и ввод первого массива
-    std::cin >> n;
-    int* a = new int[n];
-    for (unsigned i = 0; i < n; i++)
+    Vect a = new Vect(); //создание массивов
+    Vect* b = new Vect[2];
+    Vect c = new Vect();
+
+    //ввод массивов
+
+    std::cin >> a.l;
+    a.allocate();
+    for (uint16_t i = 0; i < a.l; i++)
         std::cin >> a[i];
 
-    unsigned m; //создание и ввод второго массива
-    std::cin >> m;
-    int* b = new int[m];
-    for (unsigned i = 0; i < m; i++)
+    std::cin >> b[0].l;
+    b[0].allocate();
+    for (uint16_t i = 0; i < b.l; i++)
         std::cin >> b[i];
 
-    unsigned k; //создание и ввод третьего массива
-    std::cin >> k;
-    int* c = new int[k];
-    for (unsigned i = 0; i < k; i++)
+    std::cin >> c.l;
+    c.allocate();
+    for (uint16_t i = 0; i < c.l; i++)
         std::cin >> c[i];
 
-    for (unsigned i = 0; i < k; i++) { //нахождение сумм простым перебором
-        bool found = false; //искать, пока не станет true
-        for (unsigned j = 0; j < n && !found; j++)
-            for (unsigned t = 0; t < m && !found; t++)
-                if (a[j] + b[t] == c[i]) //если нашли сумму, то ставим флаг
-                    found = true;
-        if (found) //сразу выводим ответ и переходим к следующему элементу
-            std::cout << "YES";
-        else
-            std::cout << "NO";
-        std::cout << std::endl;
+    //обработка
+
+    if (a.l > b.l) {std::swap(a, b);} //b всегда должен быть больше a
+
+    //максимальное число одновременно хранимых
+    uint8_t sumn;
+    uin16_t totaln = a.l * b.l, maxn = 60000000;
+    Vect* sum;
+    if (totaln > maxn) {
+        sum = new Vect[2];
+        sum[0].l = maxn;
+        sum[1].l = totaln -
     }
-    delete[] a; //удаляем все три массива из памяти
-    delete[] b;
-    delete[] c;
+
+
+
+
+    delete a; //удаляем все три массива из памяти
+    delete b;
+    delete c;
     return 0;
 }
